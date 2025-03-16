42f2bca93bc0613bdaf0f052f35bb12b2c35fb59
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-16 18:49:06 +0100

    new: Add metadata field to the astTF type declaration

--- src/astTF.ts
+++ src/astTF.ts
@@ -1,10 +1,14 @@
 
+export type LiteralValue = string
+
 export type Integer = {
-  signed :boolean
+  signed  :boolean
+  value   :LiteralValue
 }
 
 export type Strng = {
-  multiline :boolean
+  multiline  :boolean
+  value      :LiteralValue
 }
 
 export type Literal = Integer | Strng
@@ -12,9 +16,9 @@ export type Literal = Integer | Strng
 export type Expression = Literal
 
 export type Data = {
-  value :Expression
-  read  :boolean
-  write :boolean
+  value  :Expression
+  read   :boolean
+  write  :boolean
 }
 
 export type Procedure = {
@@ -22,9 +26,10 @@ export type Procedure = {
 }
 
 export type Variable  = {
-  data    :Data
-  runtime :boolean
+  data     :Data
+  runtime  :boolean
 }
+
 
 /**
  * @description Node declared/described at the toplevel of the source code
@@ -32,7 +37,29 @@ export type Variable  = {
 export type TopLevel = Procedure | Variable
 
 /**
+ * @description Identifier that represents the target language of the AST
+ * @note The parser and code generators are responsible for creating a standard for the contents of this type.
+ * */
+export type Language = string | null
+
+/**
+ * @description Human-readable string that describes the contents of the file.
+ * */
+export type Description = string | null
+
+/**
+ * @description Metadata describing the contents of the file
+ * */
+export type Metadata = {
+  language     :Language
+  description  :Description
+}
+
+/**
  * @description Entry point of an astTF file
  * */
-export type astTF = Array<TopLevel>
+export type astTF = {
+  metadata  :Metadata
+  data      :Array<TopLevel>
+}
 
